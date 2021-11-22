5d9eb8c610829aec326d73616455cb65b87ae3b7
Emily Martinek <emily.martinek12@gmail.com>
2021-11-22 16:42:46 +0100

    validations

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,3 +1,13 @@
 class Cafe < ApplicationRecord
   belongs_to :user
+  has_many :cafe_tags
+  has_many :tags, through: :cafe_tags
+  has_many :chairs
+  has_many :reservations, through: :chairs
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :open_hour, presence: true
+  validates :close_hour, presence: true
+  validates :description, length: { minimum: 100 }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  has_many :cafes, through: :cafe_tags
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
