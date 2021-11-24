7af606c407f5aaa578eb6a97f4ed24c25b99895c
Constantin Beissel <constantinbeissel@googlemail.com>
2021-11-24 11:53:35 +0100

    cafe-form-beg

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,11 +1,32 @@
 class CafesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [ :index, :show ]
 
+  def new
+    @cafes = Cafe.new
+  end
+
   def index
     @cafes = Cafe.all
   end
 
   def show
     @cafe = Cafe.find(params[:id])
+  end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    @cafe.user = current_user
+
+    if @cafe.save
+      redirect_to @cafe, notice: 'We welcome your Cafe to Working Latte'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:name, :address, :opening_hour, :closing_hour, :description, photos: [])
   end
 end
