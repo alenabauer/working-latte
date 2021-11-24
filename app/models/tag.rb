0f66c7912d315f66fbc718b67c907031b1998b57
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-24 21:21:47 +0100

    seed tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :cafes, through: :cafe_tags
 
   validates :name, presence: true
-  validates :description, presence: true
+  # validates :description, presence: true
 end
