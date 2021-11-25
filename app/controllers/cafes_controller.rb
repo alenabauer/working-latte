6df366a4953eb97829a7bf052939256e9c88dd94
Constantin Beissel <constantinbeissel@googlemail.com>
2021-11-25 11:56:09 +0100

    cafe tags

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -23,8 +23,8 @@ class CafesController < ApplicationController
   def create
     @cafe = Cafe.new(cafe_params)
     @cafe.user = current_user
-
     if @cafe.save
+      create_cafe_tags
       redirect_to @cafe, notice: 'We welcome your Cafe to Working Latte'
     else
       render :new
@@ -35,5 +35,11 @@ class CafesController < ApplicationController
 
   def cafe_params
     params.require(:cafe).permit(:name, :address, :opening_hour, :closing_hour, :description, photos: [])
+  end
+
+  def create_cafe_tags
+    params[:cafe][:tag_ids].reject {|id| id == "" }.each do |id|
+      CafeTag.create(tag_id: id, cafe: @cafe)
+    end
   end
 end
