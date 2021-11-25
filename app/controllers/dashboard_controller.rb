f0df8258af8985b80cdc4dcf1c2c0a537999e014
Emily Martinek <emily.martinek12@gmail.com>
2021-11-25 17:59:20 +0100

    less margin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,4 @@
 class DashboardController < ApplicationController
+  def show_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cafes, only: [:index, :show, :new, :create ]
+
+  get '/dashboard', to: 'dashboard#show_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
