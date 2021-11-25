395c98bf4a559312277aad170ced092d699682b1
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-25 23:46:29 +0100

    create dashboard

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,9 +3,16 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
     @reservation.user = current_user
     @reservation.chair = Chair.find(params[:chair_id])
+    @time_slots = []
+    params[:reservation][:time_slot_ids].reject { |id| id == "" }.each do |id|
+      @time_slots << TimeSlot.find(id)
+    end
+    @reservation.date = Date.new(@time_slots.first.start_time.year, @time_slots.first.start_time.month, @time_slots.first.start_time.day )
+    @reservation.start_time = Time.new(@time_slots.first.start_time.year, @time_slots.first.start_time.month, @time_slots.first.start_time.day, @time_slots.first.start_time.hour, 0, 0)
+    @reservation.end_time = Time.new(@time_slots.last.start_time.year, @time_slots.last.start_time.month, @time_slots.last.start_time.day, @time_slots.last.start_time.hour + 1, 0, 0)
     if @reservation.save
       create_reservation_time_slots
-      redirect_to cafe_path(@reservation.chair.cafe), notice: 'Your reservation was successful.'
+      redirect_to dashboard_path, notice: 'Your reservation was successful.'
     else
       # render
     end
