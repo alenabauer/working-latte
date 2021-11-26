2ec74c8fdf8ce3fad597d101910990919d274f7d
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-26 14:46:53 +0100

    still working on reservations

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -1,2 +1,14 @@
 class ChairsController < ApplicationController
+  def fetch_time_slots
+    @chair = Chair.find(params[:id])
+    @year = params[:year]
+    @month = params[:month]
+    @day = params[:day]
+    @time_slots = TimeSlot.where(chair: @chair).select do |slot|
+      slot.start_time.year == @year && slot.start_time.month == @month && slot.start_time.day == @day
+    end
+    respond_to do |format|
+      format.js render json: { data: @time_slots }
+    end
+  end
 end
