c1515ad028715ad9d90a7a62e5d73564a6a0aad2
paula garcia <paula.garciadv@gmail.com>
2021-11-29 12:28:20 +0100

    geolocation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if Rails.env.development?
+      ip = "192.168.1.201"
+      results = Geocoder.search(ip)
+      @cafes = Cafe.near(results.first.coordinates, 4)
+    else
+      @cafes = Cafe.near(request.location.coordinates, 4)
+    end
   end
 end
